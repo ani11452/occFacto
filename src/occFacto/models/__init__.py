2496089e6957508cf24466af506aac172797ef2f
William Pan <willpan@stanford.edu>
2023-11-16 17:16:45 -0800

    occFacto v3.4

---
+++ src/occFacto/models/__init__.py
@@ -0,0 +1,6 @@
+from .anchored_diffusion import AnchoredDiffusion
+from .anchor_gen import AnchorDiffAE
+from .attention import TransformerNet
+from .flow import build_latent_flow
+from .part_encoders import PartAligner, PartEncoder
+from .pointnet import PointNet, PointNetV2, PointNetV3, PointNetV2VAE, PointNetVAE
--- src/occFacto/utils/__init__.py
+++ src/occFacto/utils/__init__.py
@@ -1,1 +1,3 @@
 from .loggers import *
+from . import diffusion_utils
+from . import language_util
