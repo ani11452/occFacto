a90653c5ca2a12a2610acece59d974039fdce29f
William Pan <willpan@stanford.edu>
2023-11-16 16:55:20 -0800

    occFacto v3.1

--- src/setup.py
+++ src/setup.py
@@ -5,5 +5,5 @@ path = os.path.join(os.path.dirname(__file__),"occFacto")
 setup(
     name="occFacto",
     packages=find_packages(path),
-    package_dir={'': "."},
+    package_dir={'': "occFacto"},
 )
